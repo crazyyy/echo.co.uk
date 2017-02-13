f2e101ecc5c1c29d168c4c8caf2d58d85afa9177
crazyyy <crazyyy@gmail.com>
2017-02-13 06:12:15 +0200

    wp updated

--- wordpress/wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wordpress/wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit4bece919afb2ef52ec98278a50c6d52d::getLoader();
+return ComposerAutoloaderInit6635f2a2d429a0e1dac5acb24f324193::getLoader();
--- wordpress/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wordpress/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit1d8513c4a267a376f259e44586ab04ca::getLoader();
+return ComposerAutoloaderInit1737d231749789f176f75ebca9e3b883::getLoader();
