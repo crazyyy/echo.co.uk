84c8d280e559dbbc4105050bda8db2bd2133cf4d
crazyyy <crazyyy@gmail.com>
2017-02-13 06:04:52 +0200

    wordpress upd

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_NAME', 'DBNAME');
-define('DB_USER', 'DBUSER');
-define('DB_PASSWORD', 'DBPASS');
+define('DB_NAME', 'echo');
+define('DB_USER', 'echo');
+define('DB_PASSWORD', 'echo');
 
 define('DB_HOST', 'localhost');
 
---
+++ wordpress/wp-content/themes/wp-echo/js/inline_0.js
@@ -0,0 +1,2 @@
+document.documentElement.className+=" js",Function("/*@cc_on return document.documentMode===10@*/")()&&(document.documentElement.className+=" ie10");
+//# sourceMappingURL=maps/inline_0.js.map
