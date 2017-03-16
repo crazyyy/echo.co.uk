58aa40c60c0e77215d12ad952ce6a912f2095154
crazyyy <crazyyy@gmail.com>
2017-03-16 22:51:33 +0200

    new html version

--- assets/js/inline_0.js
+++
@@ -1,6 +0,0 @@
-
-		document.documentElement.className+=' js';
-		if (Function('/*@cc_on return document.documentMode===10@*/')()){
-			document.documentElement.className+=' ie10';
-		}
-	
--- html/js/inline_0.js
+++
@@ -1,2 +0,0 @@
-document.documentElement.className+=" js",Function("/*@cc_on return document.documentMode===10@*/")()&&(document.documentElement.className+=" ie10");
-//# sourceMappingURL=maps/inline_0.js.map
